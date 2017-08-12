be457746058fac74ac63df48e587d77e6b02cc61
gvozdb <i@owl.kz>
2017-08-12 15:16:51 +0500

    1.0.0-beta

--- core/components/modextralayout/plugins/melonmodxinit.class.php
+++ core/components/modextralayout/plugins/melonmodxinit.class.php
@@ -7,7 +7,7 @@ class melOnMODXInit extends melPlugin
 {
     public function run()
     {
-        $this->mel->tools->systemMapExtend(array(
+        $this->map(array(
             'modResource' => array(
                 'composites' => array(
                     'melObjects' => array(
@@ -25,5 +25,37 @@ class melOnMODXInit extends melPlugin
                 ),
             ),
         ));
+    }
+    
+    /**
+     * @param array $map
+     *
+     * @return bool
+     */
+    public function map(array $map = array())
+    {
+        foreach ($map as $class => $data) {
+            $this->modx->loadClass($class);
+
+            foreach ($data as $tmp => $fields) {
+                if ($tmp == 'fields') {
+                    foreach ($fields as $field => $value) {
+                        foreach (array('fields', 'fieldMeta', 'indexes') as $key) {
+                            if (isset($data[$key][$field])) {
+                                $this->modx->map[$class][$key][$field] = $data[$key][$field];
+                            }
+                        }
+                    }
+                } elseif ($tmp == 'composites' || $tmp == 'aggregates') {
+                    foreach ($fields as $alias => $relation) {
+                        if (!isset($this->modx->map[$class][$tmp][$alias])) {
+                            $this->modx->map[$class][$tmp][$alias] = $relation;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
     }
 }
