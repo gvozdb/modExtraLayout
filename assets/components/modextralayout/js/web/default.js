b6c76f783a0025cc3f25886f2818101eb9f60f3d
gvozdb <pavelgvozdb@yandex.ru>
2018-12-05 16:50:31 +0600

    1.0.0-beta

--- assets/components/modextralayout/js/web/default.js
+++ assets/components/modextralayout/js/web/default.js
@@ -1,14 +1,18 @@
 (function () {
     function modExtraLayout(options) {
-        ['actionUrl'].forEach(function (val, i, arr) {
-            if (typeof(options[val]) == 'undefined' || options[val] == '') {
-                console.error('[modExtraLayout] Bad config.', arr);
-                return;
-            }
-        });
         var self = this;
         self['initialized'] = false;
         self['running'] = false;
+        self['fatal'] = false;
+        ['actionUrl'].forEach(function (val, i, arr) {
+            if (typeof(options[val]) === 'undefined' || options[val] === '') {
+                console.error('[modExtraLayout] Bad config.', arr);
+                self['fatal'] = true;
+            }
+        });
+        if (self['fatal']) {
+            return;
+        }
 
         /**
          * @type {object}
@@ -106,93 +110,6 @@
          */
         self.Submit = {
             timestamp: 0,
-            // before: function () {
-            //     /**
-            //      *
-            //      */
-            //     // if (self.sendData.params['action'] == 'object/vote') {
-            //     //     // Реализуем оптимистичный интерфейс
-            //     //     var $button = self.sendData['$element'];
-            //     //     var $last = self.sendData['$last'];
-            //     //     var $object = $button.closest(self.selectors['voteObject']);
-            //     //     var $number = $button.find(self.selectors['voteNumber']);
-            //     //     var number = $number.text();
-            //     //     number = parseInt(number.replace(' ', '').replace(',', ''));
-            //     //
-            //     //     $object.find(self.selectors['voteButton']).each(function (idx, el) {
-            //     //         var $btn = $(el);
-            //     //         if ($button.get(0) != $btn.get(0)) {
-            //     //             if ($btn.hasClass(self.classes['voteButtonActive'])) {
-            //     //                 $btn.removeClass(self.classes['voteButtonActive']); // .removeAttr('data-nkn-vote-tmp')
-            //     //                 var $num = $btn.find(self.selectors['voteNumber']);
-            //     //                 var num = $num.text();
-            //     //                 num = parseInt(num.replace(' ', '').replace(',', ''));
-            //     //                 $num.text(self.Tools.number_format((num - 1), 0, '.', ' '));
-            //     //             }
-            //     //         }
-            //     //     });
-            //     //     $button.toggleClass(self.classes['voteButtonActive']).data('nkn-vote-tmp', 1);
-            //     //     if ($button.hasClass(self.classes['voteButtonActive'])) {
-            //     //         $number.text(self.Tools.number_format((number + 1), 0, '.', ' '));
-            //     //     } else {
-            //     //         $number.text(self.Tools.number_format((number - 1), 0, '.', ' '));
-            //     //     }
-            //     //
-            //     //     // if (!!$last && $last.length && $last.get(0) != $button.get(0)) {
-            //     //     //     console.log('before submit $last', $last);
-            //     //     //     console.log('before submit $last.hasClass(self.classes[voteButtonActive]) 1', $last.hasClass(self.classes['voteButtonActive']));
-            //     //     //     $last.toggleClass(self.classes['voteButtonActive']);
-            //     //     //     console.log('before submit $last.hasClass(self.classes[voteButtonActive]) 2', $last.hasClass(self.classes['voteButtonActive']));
-            //     //     // }
-            //     // }
-            // },
-            // after: function (response) {
-            //     /**
-            //      *
-            //      */
-            //     // if (self.sendData.params['action'] == 'object/vote') {
-            //     //     var $button = self.sendData['$element'];
-            //     //     var $object = $button.closest(self.selectors['voteObject']);
-            //     //
-            //     //     // При безуспешном запросе
-            //     //     if (!response['success']) {
-            //     //         var $last = self.sendData['$last'];
-            //     //
-            //     //         // 1) Переключаем класс активной кнопки
-            //     //         $button.toggleClass(self.classes['voteButtonActive']).removeAttr('data-nkn-vote-tmp');
-            //     //
-            //     //         // 2) Возвращаем класс активной кнопки последнему элементу
-            //     //         if (!!$last && $last.length) {
-            //     //             $last.toggleClass(self.classes['voteButtonActive']);
-            //     //         }
-            //     //     }
-            //     //     // При успешном запросе
-            //     //     else {
-            //     //         var $rating = $object.find(self.selectors['voteRating']);
-            //     //         var $stripe = $object.find(self.selectors['voteStripe']);
-            //     //
-            //     //         // 1) Удаляем обозначение временного назначения класса
-            //     //         $button.removeAttr('data-nkn-vote-tmp');
-            //     //
-            //     //         // 2) Ставим новое значение рейтинга
-            //     //         if (typeof(response.data['rating']) != 'undefined') {
-            //     //             var rating_old = parseFloat($rating.text().replace(' ', ''));
-            //     //             var rating_new = parseFloat(response.data['rating']);
-            //     //             console.log('rating_old', rating_old);
-            //     //             console.log('rating_new', rating_new);
-            //     //
-            //     //             // $rating.text(rating_new);
-            //     //
-            //     //             self.Tools.animateNumbers($rating, rating_old, rating_new, 700);
-            //     //         }
-            //     //
-            //     //         // 3) Ставим новую длину полосы
-            //     //         if (typeof(response.data['rating']) != 'undefined') {
-            //     //             $stripe.css({minWidth: response.data['rating'] + '%'});
-            //     //         }
-            //     //     }
-            //     // }
-            // },
             post: function (beforeCallback, afterCallback) {
                 if (!self.sendData['params'] || !self.sendData.params['action']) {
                     return;
