f974c52f89371b097d7a7d8f277763ea31dd9202
gvozdb <pavelgvozdb@yandex.ru>
2019-07-22 11:07:32 +0600

    1.0.0-beta

---
+++ core/components/modextralayout/lexicon/ru/common.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+// Табы
+$_lang['mel_tab_main'] = 'Основное';
+
+// // Названия столбцов
+$_lang['mel_grid_id'] = 'ID';
+$_lang['mel_grid_idx'] = 'idx';
+$_lang['mel_grid_actions'] = 'Действия';
+
+// Кнопки
+$_lang['mel_button_create'] = 'Добавить';
+$_lang['mel_button_update'] = 'Редактировать';
+$_lang['mel_button_enable'] = 'Включить';
+$_lang['mel_button_enable_multiple'] = 'Включить выбранное';
+$_lang['mel_button_disable'] = 'Выключить';
+$_lang['mel_button_disable_multiple'] = 'Выключить выбранное';
+$_lang['mel_button_remove'] = 'Удалить';
+$_lang['mel_button_remove_multiple'] = 'Удалить выбранное';
+
+// Подтверждения
+$_lang['mel_confirm_other'] = 'Вы уверены?';
+$_lang['mel_confirm_remove'] = 'Вы уверены, что хотите удалить объект(-ы)?';
+
+// Ошибки общие
+$_lang['mel_err_unexpected'] = 'Непредвиденная ошибка.';
+$_lang['mel_err_required'] = 'Это поле необходимо заполнить.';
+$_lang['mel_err_unique'] = 'Это поле должно быть уникальным.';
+$_lang['mel_err_ns'] = 'Объект не указан.';
+$_lang['mel_err_nf'] = 'Объект не найден.';
+
+// ComboBox
+$_lang['mel_combo_list_empty'] = 'Выпадающий список пуст...';
--- core/components/modextralayout/lexicon/ru/default.inc.php
+++ core/components/modextralayout/lexicon/ru/default.inc.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'common.inc.php';
 include_once 'setting.inc.php';
 
 // Основное
@@ -7,12 +8,8 @@ $_lang['mel_menu_desc'] = 'Заготовка для разработки пак
 
 // Табы
 $_lang['mel_tab_objects'] = 'Объекты';
-$_lang['mel_tab_main'] = 'Основное';
 
 // Названия столбцов
-$_lang['mel_grid_id'] = 'ID';
-$_lang['mel_grid_idx'] = 'idx';
-$_lang['mel_grid_actions'] = 'Действия';
 $_lang['mel_grid_parent'] = 'Родительский ресурс';
 $_lang['mel_grid_group'] = 'Группа';
 $_lang['mel_grid_name'] = 'Название';
@@ -33,28 +30,14 @@ $_lang['mel_window_object_create'] = 'Добавить объект';
 $_lang['mel_window_object_update'] = 'Редактировать объект';
 
 // Кнопки
-$_lang['mel_button_create'] = 'Добавить';
-$_lang['mel_button_update'] = 'Редактировать';
-$_lang['mel_button_enable'] = 'Включить';
-$_lang['mel_button_enable_multiple'] = 'Включить выбранное';
-$_lang['mel_button_disable'] = 'Выключить';
-$_lang['mel_button_disable_multiple'] = 'Выключить выбранное';
-$_lang['mel_button_remove'] = 'Удалить';
-$_lang['mel_button_remove_multiple'] = 'Удалить выбранное';
+$_lang['mel_button_'] = '';
 
 // Подтверждения
-$_lang['mel_confirm_remove'] = 'Вы уверены, что хотите удалить объект(-ы)?';
+$_lang['mel_confirm_'] = '';
 
 // Ошибки конкретизированные
 $_lang['mel_err_required_name'] = 'Необходимо указать название.';
 $_lang['mel_err_unique_name'] = 'Название должно быть уникальным.';
-
-// Ошибки общие
-$_lang['mel_err_unexpected'] = 'Непредвиденная ошибка.';
-$_lang['mel_err_required'] = 'Это поле необходимо заполнить.';
-$_lang['mel_err_unique'] = 'Это поле должно быть уникальным.';
-$_lang['mel_err_ns'] = 'Объект не указан.';
-$_lang['mel_err_nf'] = 'Объект не найден.';
 
 // Успехи
 $_lang['mel_success_'] = '';
@@ -63,7 +46,7 @@ $_lang['mel_success_'] = '';
 $_lang['mel_message_'] = '';
 
 // ComboBox
-$_lang['mel_combo_list_empty'] = 'Выпадающий список пуст...';
+$_lang['mel_combo_'] = '';
 
 // Группы
 $_lang['mel_group_group_1'] = 'Group 1';
